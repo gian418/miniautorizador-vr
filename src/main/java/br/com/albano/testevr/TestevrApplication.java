84b0e867c05890a3c3494b2acc5abcd8ea81416b
Giancarlo Haack Albano <gian418@gmail.com>
2024-06-07 21:30:27 -0300

    Criacao do repo

---
+++ src/main/java/br/com/albano/testevr/TestevrApplication.java
@@ -0,0 +1,13 @@
+package br.com.albano.testevr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestevrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestevrApplication.class, args);
+	}
+
+}
